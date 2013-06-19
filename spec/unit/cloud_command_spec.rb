747fa1529f07ba217d2358077ec48a1603bdd74a
Mukta Aphale <Mukta.Aphale@Clogeny.com>
2013-06-19 23:03:00 +0530

    Cleaned up code

--- spec/unit/cloud_command_spec.rb
+++ spec/unit/cloud_command_spec.rb
@@ -8,28 +8,30 @@ describe Chef::Knife::Cloud::Command do
   before do
     @app = App.new
     @service = Object.new
-    @instance = Chef::Knife::Cloud::ServerCreateCommand.new(@app, @service)
+    @instance = Chef::Knife::Cloud::Command.new(@app, @service)
   end
 
-  it "Should ask for compulsory properties to be set" do
+  it "should ask for compulsory properties to be set" do
     expect {Chef::Knife::Cloud::Command.new}.to raise_error(ArgumentError)
   end
 
-  it "Should create instance with correct params" do
-    pending "Revisit compulsory params"
-    @instance.app.should == 'app'
-    @instance.service.should == 'service'
+  it "should create instance with correct params" do
+    @instance.service.class.should == Object
     @instance.ui.class.should == Chef::Knife::UI
   end
 
-  it "Should run with correct method calls" do
+  it "should run with correct method calls" do
     @instance.stub(:exec_command)
     @instance.should_receive(:validate!).ordered
     @instance.should_receive(:before_handler).ordered
     @instance.should_receive(:exec_command).ordered
     @instance.should_receive(:after_handler).ordered
-    @instance.should_receive(:custom_arguments).ordered
+    #@instance.should_receive(:custom_arguments).ordered
     @instance.run
+  end
+
+  it "should raise exception to override exec_command" do
+    expect {@instance.run}.to raise_error(Chef::Exceptions::Override, "You must override exec_command in #{@instance.to_s}")
   end
 
 end
--- spec/unit/server_create_command_spec.rb
+++ spec/unit/server_create_command_spec.rb
@@ -11,36 +11,56 @@ describe Chef::Knife::Cloud::ServerCreateCommand do
     @instance = Chef::Knife::Cloud::ServerCreateCommand.new(@app, @service)
   end
 
-  it "Should ask for compulsory properties to be set" do
+  it "should ask for compulsory properties to be set" do
     expect {Chef::Knife::Cloud::ServerCreateCommand.new}.to raise_error(ArgumentError)
   end
 
-  it "Should run with correct method calls" do
+  it "should run with correct method calls" do
     @instance.stub(:create_server_dependencies)
     @instance.stub(:create)
+    @instance.stub(:bootstrap)
     @instance.should_receive(:validate!).ordered
     @instance.should_receive(:before_handler).ordered
-
-    @instance.should_receive(:exec_command).ordered
     @instance.should_receive(:create_server_dependencies).ordered
     @instance.should_receive(:create).ordered
-
     @instance.should_receive(:bootstrap).ordered
-    @instance.should_receive(:before_bootstrap).ordered
-    @instance.should_receive(:after_bootstrap).ordered
-
     @instance.should_receive(:after_handler).ordered
-
-    @instance.should_receive(:custom_arguments).ordered
+    #@instance.should_receive(:custom_arguments).ordered
     @instance.run
   end
 
-  it "Should delete_server_dependencies on failure executing command" do
+  it "should delete_server_dependencies on failure creating server" do
     @instance.stub(:create_server_dependencies)
     @instance.stub(:create).and_raise(Chef::Knife::Cloud::CloudExceptions::ServerCreateError)
 
     @instance.should_receive(:delete_server_dependencies)
     @instance.ui.should_receive(:fatal)
     expect {@instance.run}.to raise_error(Chef::Knife::Cloud::CloudExceptions::ServerCreateError)
+  end
+
+  it "should delete_server_dependencies on failure creating server dependencies" do
+    @instance.stub(:create_server_dependencies).and_raise(Chef::Knife::Cloud::CloudExceptions::ServerCreateDependenciesError)
+    @instance.stub(:create)
+
+    @instance.should_receive(:delete_server_dependencies)
+    @instance.ui.should_receive(:fatal)
+    expect {@instance.run}.to raise_error(Chef::Knife::Cloud::CloudExceptions::ServerCreateDependenciesError)
+  end
+
+  it "should raise exception to override create_server_dependencies" do
+    expect {@instance.run}.to raise_error(Chef::Exceptions::Override, "You must override create_server_dependencies in #{@instance.to_s} to create dependencies required for server creation.")
+  end
+
+  it "should raise exception to override create server method" do
+    @instance.stub(:create_server_dependencies)
+    expect {@instance.run}.to raise_error(Chef::Exceptions::Override, "You must override create in #{@instance.to_s} for server creation.")
+  end
+
+  it "should raise exception to override delete_server_dependencies" do
+    expect {@instance.delete_server_dependencies}.to raise_error(Chef::Exceptions::Override, "You must override delete_server_dependencies in #{@instance.to_s} to remove dependencies created before server creation.")
+  end
+
+  it "" do
+
   end
 end
